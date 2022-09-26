1ed527c322b97346aa314279c29b0db27d48ef6c
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-09-26 16:53:31 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let setColorId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+refs.stopBtn.setAttribute('disabled', true);
+
+function onStartBtnClick() {
+    setColorId = setInterval(() => {
+        refs.bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    refs.startBtn.setAttribute('disabled', true);
+    if (refs.stopBtn.hasAttribute('disabled')) {
+        refs.stopBtn.removeAttribute('disabled');
+    }
+}
+
+function onStopBtnClick() {
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', true);
+    clearInterval(setColorId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+    
